20ee1c794b61e8ba8785cafcb058673c899ff144
Willian Marciano <willian_marciano@hotmail.com>
2022-05-10 13:18:23 -0300

    Recuperando um script Incluido LogandoNoSistema() que utiliza Selenium WebDriver

--- tests/ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
+++ tests/ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using System.IO;
 using System.Reflection;
 using Xunit;
@@ -33,6 +34,22 @@ namespace ByteBank.WebApp.Testes
             //Assert
             Assert.Contains("Login", driver.PageSource);
             Assert.Contains("Home", driver.PageSource);
+        }
+
+        [Fact]
+        public void LogandoNoSistema()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            driver.Navigate().GoToUrl("https://localhost:7155/");
+            driver.Manage().Window.Size = new System.Drawing.Size(1280, 672);
+            driver.FindElement(By.LinkText("Login")).Click();
+            driver.FindElement(By.Id("Email")).Click();
+            driver.FindElement(By.Id("Email")).SendKeys("admin@email.com");
+            driver.FindElement(By.Id("Senha")).Click();
+            driver.FindElement(By.Id("Senha")).SendKeys("senha01");
+            driver.FindElement(By.Id("btn-logar")).Click();
         }
     }
 }
