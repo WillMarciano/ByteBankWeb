b279ba5a7e79964d537056b7b170517ee244289a
Willian Marciano <willian_marciano@hotmail.com>
2022-05-10 12:23:21 -0300

    Criando Primeiro teste WebDriver

---
+++ tests/ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace ByteBank.WebApp.Testes
+{
+    public class NavegandoNaPaginaHome
+    {
+        [Fact]
+        public void CarregaPaginaHomeEVerificaTituloDaPagina()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            //Act
+            driver.Navigate().GoToUrl("https://localhost:7155");
+
+            //Assert
+            Assert.Contains("WebApp", driver.Title);
+        }
+
+        [Fact]
+        public void CarregaPaginaHomeVerificaExistenciaLinkLoginEHomet()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            //Act
+            driver.Navigate().GoToUrl("https://localhost:7155");
+
+            //Assert
+            Assert.Contains("Login", driver.PageSource);
+            Assert.Contains("Home", driver.PageSource);
+        }
+    }
+}
